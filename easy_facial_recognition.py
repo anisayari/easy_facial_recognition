39a90617ff95110dcbc747ef25d5f6613a70edb4
anisayari <anis.ayari.perso@gmail.com>
2019-10-18 12:26:00 +0200

    arrangement

--- easy_facial_recognition.py
+++ easy_facial_recognition.py
@@ -1,9 +1,16 @@
-#Code Anis - Defend Intelligence
+# Code Anis - Defend Intelligence
 import cv2
 import dlib
 import PIL.Image
 import numpy as np
 from imutils import face_utils
+import argparse
+from pathlib import Path
+import os
+import ntpath
+
+parser = argparse.ArgumentParser(description='Easy Facial Recognition App')
+parser.add_argument('-i', '--input', type=str, required=True, help='directory of input known faces')
 
 print('[INFO] Starting System...')
 print('[INFO] Importing pretrained model..')
@@ -13,6 +20,7 @@ face_encoder = dlib.face_recognition_model_v1("pretrained_model/dlib_face_recogn
 face_detector = dlib.get_frontal_face_detector()
 print('[INFO] Importing pretrained model..')
 
+
 def transform(image, face_locations):
     coord_faces = []
     for face in face_locations:
@@ -20,6 +28,7 @@ def transform(image, face_locations):
         coord_face = max(rect[0], 0), min(rect[1], image.shape[1]), min(rect[2], image.shape[0]), max(rect[3], 0)
         coord_faces.append(coord_face)
     return coord_faces
+
 
 def encode_face(image):
     face_locations = face_detector(image, 1)
@@ -33,17 +42,18 @@ def encode_face(image):
         shape = face_utils.shape_to_np(shape)
         landmarks_list.append(shape)
     face_locations = transform(image, face_locations)
-    return face_encodings_list,face_locations,landmarks_list
+    return face_encodings_list, face_locations, landmarks_list
 
-def easy_face_reco(frame,known_face_encodings, known_face_names):
+
+def easy_face_reco(frame, known_face_encodings, known_face_names):
     rgb_small_frame = frame[:, :, ::-1]
     # ENCODING FACE
-    face_encodings_list,face_locations_list,landmarks_list = encode_face(rgb_small_frame)
+    face_encodings_list, face_locations_list, landmarks_list = encode_face(rgb_small_frame)
     face_names = []
     for face_encoding in face_encodings_list:
         if len(face_encoding) == 0:
             return np.empty((0))
-        #CHECK DISTANCE BETWEEN KNOWN FACES AND FACES DETECTED
+        # CHECK DISTANCE BETWEEN KNOWN FACES AND FACES DETECTED
         vectors = np.linalg.norm(known_face_encodings - face_encoding, axis=1)
         tolerance = 0.6
         result = []
@@ -62,24 +72,34 @@ def easy_face_reco(frame,known_face_encodings, known_face_names):
     for (top, right, bottom, left), name in zip(face_locations_list, face_names):
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 255, 0), cv2.FILLED)
-        cv2.putText(frame, name, (left + 2, bottom - 2), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 0), 1)
+        cv2.putText(frame, name, (left + 2, bottom - 2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
 
     for shape in landmarks_list:
         for (x, y) in shape:
             cv2.circle(frame, (x, y), 1, (255, 0, 255), -1)
 
+
 if __name__ == '__main__':
+    args = parser.parse_args()
+
     print('[INFO] Importing faces...')
-    face_to_encode_path=['input/anis.png','input/zuckerberg.png']
+    face_to_encode_path = Path(args.input)
+    files = [file_ for file_ in face_to_encode_path.rglob('*.jpg')]
+
+    for file_ in face_to_encode_path.rglob('*.png'):
+        files.append(file_)
+    if len(files)==0:
+        raise ValueError('No faces detect in the directory: {}'.format(face_to_encode_path))
+    known_face_names = [os.path.splitext(ntpath.basename(file_))[0] for file_ in files]
+
     known_face_encodings = []
-    for face_to_encode_path in face_to_encode_path:
-        image = PIL.Image.open(face_to_encode_path)
+    for file_ in files:
+        image = PIL.Image.open(file_)
         image = np.array(image)
         face_encoded = encode_face(image)[0][0]
         known_face_encodings.append(face_encoded)
-    known_face_names = ["Anis",'Zuckerberg']
-    print('[INFO] Faces well imported')
 
+    print('[INFO] Faces well imported')
     print('[INFO] Starting Webcam...')
     video_capture = cv2.VideoCapture(0)
     print('[INFO] Webcam well started')
@@ -87,7 +107,7 @@ if __name__ == '__main__':
     while True:
         ret, frame = video_capture.read()
         easy_face_reco(frame, known_face_encodings, known_face_names)
-        cv2.imshow('Live Reconnaissance Faciale App', frame)
+        cv2.imshow('Easy Facial Recognition App', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     print('[INFO] Stopping System')
